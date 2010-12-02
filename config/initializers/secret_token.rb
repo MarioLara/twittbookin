edfb2d7b937716f32d61b0ceff14f500319fdc1f
Mario Lara <forevert_@hotmail.com>
2010-12-02 17:16:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittbookin::Application.config.secret_token = 'ea738adc97d48f41d4d2a5238e2a47d75a6b3cc317cc1c0841591f931574a6361c2c7fe912ecd2eab8053b3aedc697528aa8eed72257bc9424200f740cdd161c'
